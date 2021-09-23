6ebe7147f06561c2dfe9d7487110e4d5f4221338
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-09-23 20:28:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 let a = prompt("Введите число a от 1 до 100");
 let i = 1;
+let j = 0;
+document.write(a+"=");
 while ( i <a) {
-  
+   
       if (a%2==0){
          a=a/2;
-         document.write(2+"</br>")
+        
+         if (j>=1){
+            document.write("*"+2)
+         }else{
+            document.write(2)
+         }
          }
          else if (a%3==0){
          a=a/3;
-         document.write(3+"</br>") 
+         if (j>=1){
+            document.write("*"+3)
+         }else{
+            document.write(3)
+         }
          }
          else if (a%5==0){
          a=a/5;
-         document.write(5+"</br>")
+         if (j>=1){
+            document.write("*"+5)
+         }else{
+            document.write(5)
+         }
          }
          else if (a%7==0){
          a=a/7;
-         document.write(7+"</br>")
+         if (j>=1){
+            document.write("*"+7)
+         }else{
+            document.write(7)
+         }
          }
         
-   
-  i++;
+         j++;
+         i++;
 }
