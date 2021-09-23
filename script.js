7c57d661bbeffe30c3012e0f6db08e73bbb65411
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-09-23 21:09:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
 let a = prompt("Введите число a от 1 до 100");
 let i = 1;
 let j = 0;
-document.write(a+"=");
-while ( i <a) {
-   
-      if (a%2==0){
-         a=a/2;
-        
-         if (j>=1){
-            document.write("*"+2)
-         }else{
-            document.write(2)
-         }
-         }
-         else if (a%3==0){
-         a=a/3;
-         if (j>=1){
-            document.write("*"+3)
-         }else{
-            document.write(3)
-         }
-         }
-         else if (a%5==0){
-         a=a/5;
-         if (j>=1){
-            document.write("*"+5)
-         }else{
-            document.write(5)
-         }
-         }
-         else if (a%7==0){
-         a=a/7;
-         if (j>=1){
-            document.write("*"+7)
-         }else{
-            document.write(7)
-         }
-         }
-        
-         j++;
-         i++;
+document.write(a + "=");
+while (i < a) {
+  if (a % 2 == 0) {
+    a = a / 2;
+
+    if (j >= 1) {
+      document.write("*" + 2);
+    } else {
+      document.write(2);
+    }
+  } else if (a % 3 == 0) {
+    a = a / 3;
+    if (j >= 1) {
+      document.write("*" + 3);
+    } else {
+      document.write(3);
+    }
+  } else if (a % 5 == 0) {
+    a = a / 5;
+    if (j >= 1) {
+      document.write("*" + 5);
+    } else {
+      document.write(5);
+    }
+  } else if (a % 7 == 0) {
+    a = a / 7;
+    if (j >= 1) {
+      document.write("*" + 7);
+    } else {
+      document.write(7);
+    }
+  }
+
+  j++;
+  i++;
 }
